1c8b858d24e61796894b251dd2f5fa74c93298e8
Joel Serfoss <jserfoss@live.com>
2025-03-21 15:49:11 -0500

    Adding Copy 2D vector to 2D list solution(3304)

---
+++ 2DVector to 2DList/2DVector to 2DList/2DVector_2DList.cpp
@@ -0,0 +1,51 @@
+//Copy 2D Vector to 2D List
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+// Function template
+template <typename T>
+void Copy_Vector_To_List_2d(const vector<vector<T>>& V, list<list<T>>& L_2d)
+{
+    for (const auto& row : V)
+    {
+        list<T> temp_list;
+        for (const auto& elem : row)
+        {
+            temp_list.push_back(elem);
+        }
+        L_2d.push_back(temp_list);
+    }
+}
+
+// Display function template (for both vector<vector> and list<list>)
+template <typename T>
+void Display_2D(const T& container)
+{
+    for (const auto& row : container)
+    {
+        for (const auto& elem : row)
+        {
+            cout << " " << elem;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> V = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    list<list<int>> L_2d;
+
+    cout << "Vector:" << endl;
+    Display_2D(V);
+
+    Copy_Vector_To_List_2d(V, L_2d);
+
+    cout << "List:" << endl;
+    Display_2D(L_2d);
+
+    return 0;
+}
